137d3ef75a0ab9041db235886f45b859cff2c4ac
cristianq3 <cristianq3@gmail.com>
2023-05-16 09:17:03 -0300

    Funcion para recargar pagina cdo hace logout

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -4,16 +4,16 @@ let btnAdministrador = document.getElementById("btnAdministrador");
 let userLogeado = JSON.parse(localStorage.getItem("userLogeado"));
 let btnIngresoDelNav = document.getElementById("btnIngresoDelNav");
 let btnSalirDelNav = document.getElementById("btnSalirDelNav");
-
-
 let validUsers = [
   { name: "Admin", email: "admin@pixelgames.com", password: "abc123" },
 ];
 
+//Manejadores de eventos
 btnIngresar.addEventListener("click", function (event) {
   event.preventDefault();
   ingresar();
 });
+btnSalirDelNav.addEventListener("click", cambiarBoolean);
 
 form.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -21,28 +21,29 @@ form.addEventListener("keypress", function (event) {
     ingresar();
   }
 });
-console.log(userLogeado);
 
 function comprobarLocalstorage(userLogeado) {
   //si Esta logueado:
-  if ((userLogeado === true)) {
-
-    btnSalirDelNav.className ="btn d-block";
-    btnIngresoDelNav.className="btn d-none";
-
-
+  if (userLogeado === true) {
+    //Mostrar Boton Salir, Ocultar Ingresar
+    btnSalirDelNav.className = "btn d-block";
+    btnIngresoDelNav.className = "btn d-none";
     //Mostrar boton administrador en Navbar
     btnAdministrador.className = "nav-item d-block";
-  
   } else {
-
-
-//Ocultar Administrador en el Navbar
+    //Ocultar Administrador en el Navbar
     btnAdministrador.className = "nav-item d-none";
+    btnSalirDelNav.className = "btn d-none";
   }
 }
 
+//Funcion cambiar variable si está log/desloguear
 
+function cambiarBoolean() {
+  userLogeado = false;
+  localStorage.setItem("userLogeado", JSON.stringify(userLogeado));
+  location.reload();
+}
 
 //Comprobar si esta logueado.
 
@@ -71,10 +72,8 @@ function ingresar() {
   }
 }
 
-comprobarLocalstorage (userLogeado);
+comprobarLocalstorage(userLogeado);
 
 // boton ingresar con display block cdo este en true la variable.
 
-
 // boton salir con display none cuando la variable este en true.
-
